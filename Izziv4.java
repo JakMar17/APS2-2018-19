2757572210f235594f65146a7d2f87ef6affa7ef
Jakob Marušič <jm8166@student.uni-lj.si>
2019-03-27 11:00:05 +0100

    ne dela

--- Izziv4.java
+++ Izziv4.java
@@ -4,34 +4,52 @@ public class Izziv4 {
     private static int [] tabela;
     private static int [] stBitov;
     private static int [] count;
-    private static int [] queue;
+
+    private static int [] urejena;
 
     public static void main(String [] args) {
         inic();
+        countingSort();
+        izpis();
     }
 
     private static void inic () {
         Scanner sc = new Scanner(System.in);
         int dolzina = sc.nextInt();
+
         tabela = new int [dolzina];
         stBitov = new int [dolzina];
-        count = new int [tabela.length];
-        queue = new int [tabela.length];
 
         for (int i = 0; i < dolzina; i++) {
             tabela[i] = sc.nextInt();
-            stBitov[i] = Integer.bitCount(tabela[i]);
+            stBitov[i] = Integer.bitCount(stBitov[i]);
         }
     }
 
     private static void countingSort() {
+        count = new int [tabela.length];
 
+        for (int i = 0; i < tabela.length; i++)
+            count[stBitov[i]]++;
+
+        for (int i = 1; i < count.length; i++)
+            count[i] += count[i-1];
+        
+        urejena = new int [tabela.length];
+        for (int i = tabela.length-1; i >= 0; i--) {
+            int element = tabela[i];
+            int bitov = stBitov[i];
+            int pozicija = count[bitov]-1;
+
+            System.out.format("(%d,%d)%n", element, pozicija);
+            urejena[pozicija] = element;
+            count[bitov]--;
+        }
     }
 
-    private static void priprava () {
-        int [] count = new int [tabela.length];
-        int [] queue = new int [tabela.length];
 
-        for (int)
+    private static void izpis() {
+        for (int i = 0; i < urejena.length; i++)
+            System.out.format("%d ", urejena[i]);
     }
 }
