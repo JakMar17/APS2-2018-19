4aff19236ea7d390c26045b0748bd0baa49c8b97
Jakob Marušič <jm8166@student.uni-lj.si>
2019-04-17 12:43:47 +0200

    koda deluje, asistentov izpis pač ne

--- Izziv7.java
+++ Izziv7.java
@@ -4,12 +4,17 @@ public class Izziv7 {
     private static Scanner sc = new Scanner(System.in);
     private static int n;
     private static int ovojnica;
+    private static int[][] vandermondovaMatrika;
     public static void main(String [] args) {
         n = sc.nextInt();
         
         int pke = pke();
-        
+        vandermond(pke);
     }
+
+    /*
+     *  od tu naprej se pojavljajo metode za racunanje PKE dane ovojnice
+     */
 
     //metoda poisce in izpise ovojnico ter vse primitivne korene enote v najdeni ovojnici
     //metoda vraca najmanjsi primitivni koren enote najdene ovojnice
@@ -33,18 +38,6 @@ public class Izziv7 {
         return najmanjsiPKE;
     }
 
-    //metoda vraca prvo prastevilo vecje ali enako kot stevilo
-    private static int praStevilo(int stevilo) {
-        if (stevilo <= 1)
-            return praStevilo(++stevilo);
-        
-        for (int i = 2; i < stevilo; i++)
-            if (stevilo % i == 0)
-                return praStevilo(++stevilo);
-        
-        return stevilo;
-    }
-
     //metoda vraca naslednji primitivni koren v dani ovojnici, ki je vecji od argumneta spodnjaMeja
     private static int naslednjiPKE (int spodnjaMeja) {
         int kandidat = -1;
@@ -59,6 +52,47 @@ public class Izziv7 {
         return kandidat;
     }
 
+
+    /*
+     *  od tu naprej se pojavljajo metode za racunanje Vandermondove matrike
+     */
+
+    //metoda izdela vandermondovo matriko
+    private static void vandermond (int pke) {
+        vandermondovaMatrika = new int [n][n];
+        int maksElement = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int notranjiElement = (int) Math.pow(pke, i);
+                int element = ostanek(notranjiElement, j);
+                if(element > maksElement)
+                    maksElement = element;
+                vandermondovaMatrika[i][j] =  element;
+            }
+        }
+
+        izpisMatrike(maksElement+"");
+    }
+
+    //metoda izpise vandermondovo matriko
+    private static void izpisMatrike(String m) {
+        int dolzinaZapisa = m.length();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                String element = vandermondovaMatrika[i][j] + "";
+                while (element.length() < dolzinaZapisa)
+                    element = " " + element;
+                System.out.format(" %s ", element);
+            }
+            System.out.println();
+        }
+    }
+    
+
+    /*
+     *  od tu naprej se pojavljajo pomozne metode
+     */
+
     //metoda vraca rezultat: stevilo^na po modulu ovojnice
     private static int ostanek (int stevilo, int na) {
         int prejsnjiClen = 1;
@@ -67,5 +101,17 @@ public class Izziv7 {
             prejsnjiClen = (prejsnjiClen * stevilo) % ovojnica;
         }
         return prejsnjiClen;
+    }
+
+    // metoda vraca prvo prastevilo vecje ali enako kot stevilo
+    private static int praStevilo(int stevilo) {
+        if (stevilo <= 1)
+            return praStevilo(++stevilo);
+
+        for (int i = 2; i < stevilo; i++)
+            if (stevilo % i == 0)
+                return praStevilo(++stevilo);
+
+        return stevilo;
     }
 }
