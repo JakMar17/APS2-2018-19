c79233c282d5ebba3d2994fc80a04c36f6224b7c
Jakob Marušič <jm8166@student.uni-lj.si>
2019-05-03 11:11:41 +0200

    zacetna verzija dn2

---
+++ Naloga2.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+//mnozenje stevil
+public class Naloga2 {
+
+    private static String ukaz;
+    private static int baza;
+    protected static Scanner sc = new Scanner (System.in);
+    
+    public static void main(String[] args) {
+        branjeVhoda();
+        
+        switch (ukaz) {
+            case "os":
+                OsnovnoSolsko os = new OsnovnoSolsko (baza);
+                break;
+            case "dv":
+                DeliInVladaj dv = new DeliInVladaj (baza);
+                break;
+            case "ka":
+                KaracubovAlgoritem ka = new KaracubovAlgoritem (baza);
+        }
+    }
+
+    private static void branjeVhoda () {
+        ukaz = sc.next();
+        baza = sc.nextInt();
+    }
+
+}
+
+class OsnovnoSolsko extends Naloga2{
+    private int baza;
+
+    public OsnovnoSolsko (int baza) {
+        this.baza = baza;
+    }
+}
+
+class DeliInVladaj extends Naloga2 {
+    private int baza;
+
+    public DeliInVladaj (int baza) {
+        this.baza = baza;
+    }
+}
+
+class KaracubovAlgoritem extends Naloga2 {
+    private int baza;
+
+    public KaracubovAlgoritem (int baza) {
+        this.baza = baza;
+    }
+}
