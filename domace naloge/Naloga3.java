8cb605f1dbdaaaede0d06bda263974c7ff99b6b2
Jakob Marušič <jm8166@student.uni-lj.si>
2019-05-28 17:28:16 +0200

    dvobarvanje dela (1/4)

--- domace naloge/Naloga3.java
+++ domace naloge/Naloga3.java
@@ -14,6 +14,8 @@ public class Naloga3 {
 
         switch(algoritem) {
             case "2c":
+                Dvobarvanje db = new Dvobarvanje(stVozlisc, graf);
+                //db.jeDvobarven();
                 break;
             case "gr":
                 break;
@@ -49,15 +51,135 @@ public class Naloga3 {
             System.out.println();
         }
     }
+
+    /*protected static boolean izpisNarascajoce(ArrayList<Povezava> array, int nivo, int mojId) {
+        if (array == null)
+            return false;
+
+        System.out.format("%d:", nivo);
+        for (int i = 0; i < array.size(); i++) {
+            int min = Integer.MAX_VALUE;
+            int indeks = -1;
+            for (int j = 0; j < array.size(); j++) {
+                if (array.get(j).njegovId(mojId) < min && array.get(j) != null) {
+                    indeks = j;
+                    min = array.get(j);
+                }
+            }
+            System.out.format(" %d", min);
+            if (indeks != -1)
+                array.set(indeks, null);
+        }
+        System.out.println();
+        return true;
+    }*/
 }
 
-class Dvobarvanje {
+class Dvobarvanje extends Naloga3{
     private Graf g;
+    private Vozlisce [] vozlisca;
     private int stVozlisc;
 
     public Dvobarvanje (int stVozlisc, Graf g) {
+        super();
         this.stVozlisc = stVozlisc;
         this.g = g;
+        vozlisca = g.getVozlisca();
+        Vozlisce prvo = vozlisca[0];
+        prvo.setObdelan(true);
+        prvo.dodajBarvo(0);
+        System.out.format("0 : 0%n");
+        imaPogoj(prvo);
+        jeDvobarven(prvo);
+    }
+
+    private boolean izpisNarascajoce (ArrayList<Vozlisce> original, int nivo) {
+        ArrayList<Vozlisce> list = new ArrayList<>();
+        list.addAll(original);
+        if (list.size() == 0)
+            return false;
+        System.out.format("%d :", nivo);
+        for (int i = 0; i < list.size(); i++) {
+            int min = Integer.MAX_VALUE;
+            int indeks = -1;
+            for (int j = 0; j < list.size(); j++) {
+                if (list.get(j) != null && min > list.get(j).getId()) {
+                    min = list.get(j).getId();
+                    indeks = j;
+                }
+            }
+            System.out.print(" " + min);
+            list.set(indeks, null);
+        }
+        System.out.println();
+        return true;
+    }
+
+    private ArrayList<Vozlisce> dobiNivo(Vozlisce v, int nivo) {
+        ArrayList<Vozlisce> list =  new ArrayList<>();
+        ArrayList <Povezava> povezave = v.getPovezave();
+
+        if (nivo == 0) {
+            for (int i = 0; i < povezave.size(); i++) {
+                Povezava p = povezave.get(i);
+                Vozlisce otrok = vozlisca[p.njegovId(v.getId())];
+                if (!otrok.getObdelan()) {
+                    list.add(otrok);
+                    otrok.setObdelan(true);
+                }
+            }
+        } else {
+            for (int i = 0; i < povezave.size(); i++) {
+                Povezava p = povezave.get(i);
+                Vozlisce otrok = vozlisca[p.njegovId(v.getId())];
+                list.addAll(dobiNivo(otrok, nivo-1));
+            }
+        }
+        return list;
+    }
+
+    private boolean jeDvobarven (Vozlisce prvo) {
+        for (int i = 0; ; i++) {
+            ArrayList<Vozlisce> list = dobiNivo(prvo, i);
+            if (list.size() == 0) {
+                System.out.format("OK");
+                return true;
+            }
+            //izpisNarascajoce(list, i+1);
+            izpisNarascajoce(list, i+1);
+            for (int j = 0; j < list.size(); j++) {
+                //System.out.println(list.get(j).getId());
+                boolean pogoj = imaPogoj(list.get(j));
+                if(!pogoj) {
+                    //System.out.println(list.get(j).getId());
+                    System.out.format("NOK");
+                    return false;
+                }
+            }
+        }
+    }
+
+    private boolean imaPogoj (Vozlisce v) {
+        //System.out.format("%d(%d)%n", v.getId(), v.getBarva());
+        ArrayList<Povezava> povezave = v.getPovezave();
+        for (int i = 0; i < povezave.size(); i++) {
+            Vozlisce otrok = vozlisca[povezave.get(i).njegovId(v.getId())];
+            if (otrok.getBarva() == -1) {
+                otrok.dodajBarvo(nasprotnaBarva(v.getBarva()));
+                //System.out.format("%d(%d) - %d(%d)\n", otrok.getId(), otrok.getBarva(), v.getId(), v.getBarva());
+            }
+            else if (otrok.getBarva() == v.getBarva()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int nasprotnaBarva (int mojaBarva) {
+        if (mojaBarva == 0)
+            return 1;
+        else
+            return 0;
     }
 }
 
@@ -91,6 +213,7 @@ class Graf {
 class Vozlisce {
     private int id;
     private int barva = -1;
+    private boolean obdelan = false;
 
     private ArrayList<Povezava> povezave;
 
@@ -107,6 +230,10 @@ class Vozlisce {
         return barva;
     }
 
+    public boolean getObdelan() {
+        return obdelan;
+    }
+
     public ArrayList<Povezava> getPovezave () {
         return povezave;
     }
@@ -118,6 +245,10 @@ class Vozlisce {
 
     public void dodajPovezavo (Povezava p) {
         povezave.add(p);
+    }
+
+    public void setObdelan (boolean obdelan) {
+        this.obdelan = obdelan;
     }
 
 }
@@ -145,5 +276,12 @@ class Povezava {
 
     public void setP2 (int p2) {
         this.p2 = p2;
+    }
+
+    public int njegovId (int mojId) {
+        if (mojId == p1)
+            return p2;
+        else
+            return p1;
     }
 }
